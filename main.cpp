26d0e777e962de5652bb2c0bcc0454884fd5c6e8
FedericoRaffaelli <91672486+FedericoRaffaelli@users.noreply.github.com>
2021-11-11 11:51:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+   int n=0, i=1, m=1, k=0, j=0;
+    cin >> n;
+    for(int i=1; i<n+2; i++){
+        j=m+k;
+        m=k;
+        k=j;
+        if(j<=n){
+            cout << j << endl;
+        }
+    }
    return 0;
 }
